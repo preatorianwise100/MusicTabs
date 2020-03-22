7b8c66c417d246b80049efc92de44e0d13db350f
preatorianwise99 <preatorianwise99@hotmail.com>
2020-03-22 15:31:23 -0300

    mas details contenidos commit

--- MusicTags/ViewController/MusicViewControl/ViewControlDetails.swift
+++ MusicTags/ViewController/MusicViewControl/ViewControlDetails.swift
@@ -11,11 +11,20 @@ import UIKit
 
 class ViewControlDetails: UIViewController {
     
+    @IBOutlet weak var LblNameAlbum: UILabel!
+    @IBOutlet weak var imagenDetails: UIImageView!
+    @IBOutlet weak var ButtonVerOnline: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setButtonVer()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+    func setButtonVer()
+    {
+        ButtonVerOnline.layer.cornerRadius = 15
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
